1c1076ceb7a179170cc51604486033e4ce48c4b0
LouisJouret <louisjouret@gmail.com>
2023-01-31 14:50:36 +0100

    add l

--- scripts/main.py
+++ scripts/main.py
@@ -14,23 +14,13 @@ print('Actions', actions)
 
 episodes = np.linspace(1, 11)
 for episode in episodes:
-    # At each begining reset the game
     state = env.reset()
-    # set done to False
     done = False
-    # set score to 0
     score = 0
-    # while the game is not finished
     while not done:
-        # visualize each step
-        # choose a random action
         action = random.choice([0, 1])
         env.render()
-        # execute the action
         n_state, reward, done, info = env.step(action)
-
-        # keep track of rewards
         score += reward
-        # set done to True when the game is terminated
 
     print('episode {} score {}'.format(episode, score))
